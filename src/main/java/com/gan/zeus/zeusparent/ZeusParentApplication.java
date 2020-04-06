a95f76e9ece2189e7c99a85f43e4565490db769f
辰星 <nirvanavrin.gwj@antfin.com>
2020-04-06 15:50:33 +0800

    first commit

---
+++ src/main/java/com/gan/zeus/zeusparent/ZeusParentApplication.java
@@ -0,0 +1,13 @@
+package com.gan.zeus.zeusparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeusParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gan/zeus/zeusparent/ZeusParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gan.zeus.zeusparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeusParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
